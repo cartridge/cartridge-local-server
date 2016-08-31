4e2ac67d7fc85ec38bc9404a350fcb2126fb0511
tawashley <trisashley@gmail.com>
2016-08-31 17:32:31 +0100

    feat: Add base browser sync config options

--- _config/task.local-server.js
+++ _config/task.local-server.js
@@ -3,10 +3,21 @@
 'use strict';
 
 function getTaskConfig(projectConfig) {
+	//Browser Sync options object
+	//https://www.browsersync.io/docs/options
+
 	var taskConfig = {
+		//Server config options
 		server: {
-			baseDir: taskConfig.baseDir
-		}
+			baseDir: projectConfig.dirs.build,
+			// directory: true, // directory listing
+			// index: "index.htm", // specific index filename
+		},
+		ghostMode: false, // Mirror behaviour on all devices
+		online: false, // Speed up startup time (When xip & tunnel aren't being used)
+		// notify: false // Turn off UI notifications
+		// browser: 'google chrome' //Set what browser to open on start - https://www.browsersync.io/docs/options#option-browser
+		// open: false // Stop browser automatically opening
 	};
 
 	return taskConfig;
